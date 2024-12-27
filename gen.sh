a6711ecb428c04a18a415268a9423ccb4de8beb9
nono <no@no.com>
2024-12-27 11:08:49 +0800

    gen code

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
 
 
-version=xxx
+version=24.05.5
 
 openapi-generator generate -i spec/openapi.${version}.json  -g lua --package-name slurmrestapi  --additional-properties packageName=slurmrestapi,packageVersion=${version} -o ./
 
